5e23e740345264a78c228a7a0814b684ccf89ea5
Yoqubjonov Dilshodbek <yoqubjonovdilshodo4@gmail.com>
2024-12-24 15:32:05 +0500

    setup prisma config

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,8 @@ import express from "express";
 import {getEnvVariable} from "./utils/dotenv";
 
 const app = express();
-const PORT = getEnvVariable("SERVER_PORT");
+const PORT = getEnvVariable("SERVER_PORT") || 8000;
+
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
