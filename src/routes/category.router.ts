f86daf17bf18ae93fdb4056dc887aab2936638fb
Yoqubjonov Dilshodbek <yoqubjonovdilshodo4@gmail.com>
2024-12-26 16:41:05 +0500

    added validete ID and multer middlewares

--- src/routes/category.router.ts
+++ src/routes/category.router.ts
@@ -1,11 +1,12 @@
 import {Router} from 'express'
 import {categoryController} from "@controllers";
+import {isValidId, upload} from "@middlewares";
 
 export const router = Router()
 
 router
     .get("/categories", categoryController.getAllCategories)
-    .get("/category/:id", categoryController.getCategorById)
-    .post("/category", categoryController.createCategory)
-    .put("/category/:id", categoryController.updateCategory)
-    .delete("/category/:id", categoryController.deleteCategory)
+    .get("/category/:id", isValidId, categoryController.getCategorById)
+    .post("/category", upload.single('workbook'), categoryController.createCategory)
+    .put("/category/:id", isValidId, upload.single('workbook'), categoryController.updateCategory)
+    .delete("/category/:id", isValidId, categoryController.deleteCategory)
