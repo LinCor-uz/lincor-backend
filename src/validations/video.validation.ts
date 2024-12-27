1b035ea766d416c808496b5cae5a4a05587c1cca
Yoqubjonov Dilshodbek <yoqubjonovdilshodo4@gmail.com>
2024-12-27 11:34:21 +0500

    deleted reviewImg_path from table

--- src/validations/video.validation.ts
+++ src/validations/video.validation.ts
@@ -3,9 +3,8 @@ import {z} from "zod";
 export const videoSchema = z.object({
     title: z.string().min(3, "Video name must be a string longer than 3 symbols"),
     description: z.string().min(10, "Video description must be a string longer than 10 symbols"),
-    reviewImg_path: z.string().optional().nullable(),
-    video_path: z.string()
-
+    video_path: z.string(),
+    categoryId: z.number().min(0)
 })
 
 export type Video = z.infer<typeof videoSchema>
