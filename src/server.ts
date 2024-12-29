c09f82cc0da0472bff64378d418a685cb52d9064
Yoqubjonov Dilshodbek <yoqubjonovdilshodo4@gmail.com>
2024-12-29 17:01:24 +0500

    added cookieParser import from cookie-parser

--- src/server.ts
+++ src/server.ts
@@ -1,25 +1,25 @@
-import express from "express"
-import {getEnvVariable} from "@utils";
-import {categoryRouter, userRouter, videoRouter} from "@router";
-
+import express from "express";
+import { getEnvVariable } from "@utils";
+import { categoryRouter, userRouter, videoRouter } from "@router";
+import cookieParser from "cookie-parser";
 
 const app = express();
 const PORT = getEnvVariable("SERVER_PORT") ?? 8000;
 
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 // base routes
 app
-    .use("/api/v1/category", categoryRouter)
-    .use("/api/v1/video", videoRouter)
-    .use("/api/v1/user", userRouter)
+  .use("/api/v1/category", categoryRouter)
+  .use("/api/v1/video", videoRouter)
+  .use("/api/v1/user", userRouter);
 
 app.use("/*", (req, res) => {
-    res.status(404).send("Page Not Found");
-})
+  res.status(404).send("Page Not Found");
+});
 
 app.listen(PORT, () => {
-    console.log(`Server started on port ${PORT}`)
-})
-
+  console.log(`Server started on port ${PORT}`);
+});
