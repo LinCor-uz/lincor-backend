1d3d5d59b595771a4fb8b2ddbe9bf4f8e077df1a
Yoqubjonov Dilshodbek <yoqubjonovdilshodo4@gmail.com>
2024-12-29 15:13:41 +0500

    changed validations to like prisma User model

--- src/validations/user.validation.ts
+++ src/validations/user.validation.ts
@@ -1,9 +1,22 @@
-import {z} from "zod";
+import { z } from "zod";
 
 export const userSchema = z.object({
-    firstname: z.string().min(3, "Firstname must be a string longer than 3 symbols"),
-    lastname: z.string().min(3, "Lastname must be a string longer than 3 symbols"),
-    phone: z.string().regex(/^\+998\d{9}$/, "Phone number must be a valid Uzbekistan number starting with +998 followed by 9 digits.")
-})
+  firstname: z
+    .string()
+    .min(3, "Firstname must be a string longer than 3 symbols"),
+  lastname: z
+    .string()
+    .min(3, "Lastname must be a string longer than 3 symbols"),
+  password: z
+    .string()
+    .min(8, "Password must be a string longer than 3 symbols"),
+  phone: z
+    .string()
+    .regex(
+      /^\+?[0-9]{9,}$/,
+      "Phone number must be a valid format with at least 9 digits"
+    ),
+  profileId: z.number().min(0).optional().nullable(),
+});
 
-export type User = z.infer<typeof userSchema>
+export type User = z.infer<typeof userSchema>;
