946ac86fb0a745c29ede0c63a3022de89d0a7a6c
Yoqubjonov Dilshodbek <yoqubjonovdilshodo4@gmail.com>
2025-01-08 22:07:55 +0500

    setup core settings

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import express from "express";
 import { getEnvVariable } from "@utils";
 import { categoryRouter, userRouter, videoRouter } from "@router";
 import cookieParser from "cookie-parser";
-
+import cors from "cors";
 const app = express();
 const PORT = getEnvVariable("SERVER_PORT") || "8000";
 
@@ -16,6 +16,14 @@ app
   .use("/api/v1/video", videoRouter)
   .use("/api/v1/user", userRouter);
 
+// core settings
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    optionsSuccessStatus: 200,
+    credentials: true,
+  })
+);
 app.use("/*", (req, res) => {
   res.status(404).send("Page Not Found");
 });
