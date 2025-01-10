688b79ea6b281cabeb4ba7a2a470fddcd8575ae0
Yoqubjonov Dilshodbek <yoqubjonovdilshodo4@gmail.com>
2025-01-10 19:37:28 +0500

    added generateToken file to index collector

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,4 @@
-export * from "./dotenv"
-export * from "./jwt"
-export * from "./error.class"
+export * from "./dotenv";
+export * from "./jwt";
+export * from "./error.class";
+export * from "./generateToken";
