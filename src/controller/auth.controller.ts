d86cf1decd4645e89d4b855976b11de467ab333f
Yoqubjonov Dilshodbek <yoqubjonovdilshodo4@gmail.com>
2025-01-10 19:34:55 +0500

    fixed much bugs on auth controller

--- src/controller/auth.controller.ts
+++ src/controller/auth.controller.ts
@@ -1,31 +1,85 @@
-import { createAccessToken, createRefreshToken, verifyToken } from "@services";
-import { sendError } from "@utils";
+import { prisma } from "@config";
+import { authService } from "@services";
+import { generateAccessToken, getEnvVariable, verifyToken } from "@utils";
 import { Request, Response } from "express";
 
-export const refreshTokenController = async (req: Request, res: Response) => {
-  try {
-    const refreshToken = req.cookies["r_AUTH"];
-    if (!refreshToken) {
-      return res.status(403).send({
-        success: false,
-        messsage: "Refresh token is required",
+export const authController = {
+  login: async (req: Request, res: Response) => {
+    try {
+      const date = req.body;
+
+      let { refreshToken, accessToken } = await authService.login(date);
+
+      res.cookie("refreshToken", refreshToken, { httpOnly: true });
+
+      res.send({ accessToken });
+    } catch (error: unknown) {
+      const err = error as Error;
+      console.log("#ERROR userLogin - ", err);
+      res.status(500).send({ success: false, error: err.message });
+    }
+  },
+
+  register: async (req: Request, res: Response) => {
+    try {
+      let data = { ...req.body, refreshToken: null };
+
+      let newUser = await authService.register(data);
+
+      res.send({
+        success: true,
+        message: "Successfuly registered",
+        newUser,
       });
+    } catch (error: unknown) {
+      const err = error as Error;
+      console.log("#ERROR userRegister - ", err);
+      res.status(500).send({ success: false, error: err.message });
+    }
+  },
+
+  refreshToken: async (res: Response, req: Request) => {
+    const { refreshToken } = req.cookies;
+    if (!refreshToken) {
+      return res
+        .status(403)
+        .json({ success: false, message: "Invalid or expired token" });
+    }
+    const user = await prisma.user.findFirst({ where: { refreshToken } });
+
+    if (!user) {
+      return res
+        .status(403)
+        .send({ success: false, message: "Invalid or expired refresh token" });
     }
 
-    const user = verifyToken(refreshToken);
+    const { payload, expired } = verifyToken(
+      refreshToken,
+      getEnvVariable("REFRESH_TOKEN_SECRET")!
+    );
 
-    const accessToken = createAccessToken(user.userId);
+    if (expired || !payload) {
+      return res.status(403).json({ message: "Invalid refresh token" });
+    }
 
-    const newRefreshToken = createRefreshToken(user.userId);
+    const accessToken = generateAccessToken(user);
+    res.send({ token: accessToken });
+  },
+  logout: async (res: Response, req: Request) => {
+    const { refreshToken } = req.cookies;
 
-    res.cookie("R_AUTH", newRefreshToken, {
-      httpOnly: true,
-      secure: true,
-      sameSite: "strict",
-    });
+    if (refreshToken) {
+      const user = await prisma.user.findFirst({ where: { refreshToken } });
 
-    res.status(200).send(accessToken);
-  } catch (error) {
-    throw new sendError("Something went wrong in Token secton", 403);
-  }
+      if (user) {
+        await prisma.user.update({
+          where: { id: user.id },
+          data: { refreshToken: " " },
+        });
+      }
+    }
+
+    res.clearCookie("refreshToken");
+    res.status(200).send({ success: true, message: "Successfuly logout" });
+  },
 };
