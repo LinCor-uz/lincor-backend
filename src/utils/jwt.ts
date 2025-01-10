998516f9a650ecb68faa2627f898a8274ed33d3e
Yoqubjonov Dilshodbek <yoqubjonovdilshodo4@gmail.com>
2025-01-10 19:38:18 +0500

    formatted

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -1,17 +1,21 @@
-import jwt from "jsonwebtoken"
-import {getEnvVariable} from "@utils";
+import jwt from "jsonwebtoken";
+import { getEnvVariable } from "@utils";
 
-const SECRET_KEY: string | null = getEnvVariable("SECRET_KEY") || "default_secret_key"
+const SECRET_KEY: string | null =
+  getEnvVariable("SECRET_KEY") || "default_secret_key";
 
-export const sign = (payload: string) => {
+export const sign = (payload: any, expiresIn: string | number) => {
+  console.log("Payload of jwt", payload);
+  console.log("Secret key of jwt", SECRET_KEY);
 
-    console.log("Payload of jwt", payload)
-    console.log("Secret key of jwt", SECRET_KEY)
-
-    return jwt.sign(payload, SECRET_KEY)
-}
+  return jwt.sign(payload, SECRET_KEY, { algorithm: "RS256", expiresIn });
+};
 
 export const verify = (token: string) => {
-    return jwt.verify(token, SECRET_KEY)
-}
-
+  try {
+    let decode = jwt.verify(token, SECRET_KEY);
+    return { payload: decode, expired: false };
+  } catch (error: any) {
+    return { payload: null, expired: error.message.include("jwt expired") };
+  }
+};
