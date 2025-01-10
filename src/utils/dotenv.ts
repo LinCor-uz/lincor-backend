e035c462e080b294bd1a414a05682b0d7fea4869
Yoqubjonov Dilshodbek <yoqubjonovdilshodo4@gmail.com>
2025-01-10 19:37:07 +0500

    formatted

--- src/utils/dotenv.ts
+++ src/utils/dotenv.ts
@@ -3,11 +3,10 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const getEnvVariable = (key: string): string | null => {
-    const value = process.env[key];
-    if (value === undefined) {
-        console.log(`Xato: ${key} muhit o'zgaruvchisi topilmadi`);
-        return null;
-    }
-    return value;
-}
-
+  const value = process.env[key];
+  if (value === undefined) {
+    console.log(`Xato: ${key} muhit o'zgaruvchisi topilmadi`);
+    return null;
+  }
+  return value;
+};
