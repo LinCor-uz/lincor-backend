964dce40f5d80da49b4dcf8cfa4fb3606b27668c
Yoqubjonov Dilshodbek <yoqubjonovdilshodo4@gmail.com>
2025-01-18 20:38:28 +0500

    changed profile validation

--- src/validations/profile.validation.ts
+++ src/validations/profile.validation.ts
@@ -1,7 +1,10 @@
 import { z } from "zod";
 
 export const profileSchema = z.object({
-  registered_at: z.date(),
+  registered_at: z.preprocess(
+    (value) => (typeof value === "string" ? new Date(value) : value),
+    z.date()
+  ),
   user_id: z.number(),
 });
 export type Profile = z.infer<typeof profileSchema>;
