8119ecfb3375d8226e5b6099a6bc2f2b43d339ae
Yoqubjonov Dilshodbek <yoqubjonovdilshodo4@gmail.com>
2025-01-18 20:37:34 +0500

    fixed user section

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -1,41 +1,41 @@
-// import { prisma } from "@config";
-// // import { userService } from "@services";
-// import { sendError } from "@utils";
-// import { Request, Response } from "express";
+import { prisma } from "@config";
+import { userService } from "@services";
+import { sendError } from "@utils";
+import { Request, Response } from "express";
 
-// export const userController = {
-//   getMe: async (req: Request, res: Response) => {
-//     try {
-//       const data = { ...req.user, profileId: req.user.id };
-//       const user = await userService.getMe(data);
+export const userController = {
+  getMe: async (req: Request, res: Response) => {
+    try {
+      const data = { ...req.user };
+      const user = await userService.getMe(data);
 
-//       res.send({
-//         success: false,
-//         data: user,
-//       });
-//     } catch (error: unknown) {
-//       const err = error as sendError;
-//       console.log("###ERROR in create USER- ", err.message);
-//       res
-//         .status(err.statusCode || 500)
-//         .send({ success: false, error: err.message });
-//     }
-//   },
+      res.send({
+        success: true,
+        data: user,
+      });
+    } catch (error: unknown) {
+      const err = error as sendError;
+      console.log("###ERROR in create USER- ", err.message);
+      res
+        .status(err.statusCode || 500)
+        .send({ success: false, error: err.message });
+    }
+  },
 
-//   profile: async (req: Request, res: Response) => {
-//     try {
-//       const userInfo = await userService.editProfile(req.body, req.user);
+  profile: async (req: Request, res: Response) => {
+    try {
+      const userInfo = await userService.editProfile(req.body, req.user);
 
-//       res.status(200).send({
-//         success: true,
-//         userInfo,
-//       });
-//     } catch (error: unknown) {
-//       const err = error as sendError;
-//       console.log("#ERROR updateUserInfo - ", err);
-//       res
-//         .status(err.statusCode || 500)
-//         .send({ success: false, error: err.message });
-//     }
-//   },
-// };
+      res.status(200).send({
+        success: true,
+        userInfo,
+      });
+    } catch (error: unknown) {
+      const err = error as sendError;
+      console.log("#ERROR updateUserInfo - ", err);
+      res
+        .status(err.statusCode || 500)
+        .send({ success: false, error: err.message });
+    }
+  },
+};
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,105 +1,113 @@
-// import { User, userSchema } from "@validations";
-// import { prisma } from "@config";
-// import { ZodError, string } from "zod";
-// import { hashPassword, sendError } from "@utils";
+import { User, userSchema } from "@validations";
+import { prisma } from "@config";
+import { ZodError, string } from "zod";
+import { hashPassword, sendError } from "@utils";
 
-// interface IProfile {
-//   id: number;
-//   password?: string;
-//   address?: string;
-//   phone?: string;
-//   ava_path?: string;
-//   user_id?: string;
-//   profileId?: number;
-//   registered_at: Date;
-// }
+interface IProfile {
+  id: number;
+  lastname: string;
+  firstname: string;
+  password?: string;
+  address?: string;
+  phone?: string;
+  ava_path?: string;
+  user_id?: string;
+  profileId?: number;
+  registered_at: Date;
+}
 
-// interface IUser {
-//   id: number;
-//   phone?: string;
-// }
+interface IUser {
+  id: number;
+  phone?: string;
+}
 
-// export const userService = {
-//   async getMe(user: IUser) {
-//     try {
-//       const me = await prisma.user.findUnique({
-//         where: { phone: user.phone },
-//         include: { profile: true },
-//       });
+export const userService = {
+  async getMe(user: IUser) {
+    try {
+      console.log("USER SERVICE GET ME INFO OF USER", user);
 
-//       return me;
-//     } catch (err) {
-//       if (err instanceof ZodError) {
-//         const validationError = err.errors.map((e) => e.message).join(", ");
-//         throw new sendError(`Validation Error: ${validationError}`, 400);
-//       }
-//       throw err;
-//     }
-//   },
+      const me = await prisma.user.findUnique({
+        where: { id: user.id },
+        include: { profile: true },
+      });
 
-//   async editProfile(data: IProfile, user: IUser) {
-//     let defaultImg: string | null = "upload/images/default.png";
-//     if (!defaultImg) {
-//       defaultImg = null;
-//     }
+      console.log(me);
 
-//     try {
-//       // Eski foydalanuvchi ma'lumotlarini olish
-//       const oldUser = await prisma.user.findUnique({
-//         where: { id: user.id },
-//       });
+      return me;
+    } catch (err) {
+      if (err instanceof ZodError) {
+        const validationError = err.errors.map((e) => e.message).join(", ");
+        throw new sendError(`Validation Error: ${validationError}`, 400);
+      }
+      throw err;
+    }
+  },
 
-//       if (!oldUser) {
-//         throw new Error("User not found");
-//       }
+  async editProfile(data: IProfile, user: IUser) {
+    try {
+      // Eski foydalanuvchi ma'lumotlarini olish
+      const oldUser = await prisma.user.findUnique({
+        where: { id: user.id },
+      });
 
-//       // Foydalanuvchining profili mavjudligini tekshirish
-//       let profile = await prisma.profile.findUnique({
-//         where: { user_id: user.id },
-//       });
+      const validateData = userSchema.safeParse(data);
 
-//       // Profil mavjud bo'lmasa, yangi profil yaratish
-//       if (!profile) {
-//         profile = await prisma.profile.create({
-//           data: {
-//             user_id: Number(user.id),
-//             address: data.address ?? undefined,
-//             ava_path: data.ava_path ?? defaultImg,
-//             registered_at: new Date(data.registered_at),
-//           },
-//         });
-//       }
+      if (!validateData.success) {
+        const validationError = validateData.error.errors
+          .map((e) => e.message)
+          .join(", ");
+        throw new sendError(`Validation Error: ${validationError}`, 400);
+      }
 
-//       // Parolni xeshlash (agar yangi parol yuborilgan bo'lsa)
-//       const hashedPassword = data.password
-//         ? await hashPassword(data.password)
-//         : oldUser.password;
+      if (!oldUser) {
+        throw new Error("User not found");
+      }
 
-//       // Yangilash ma'lumotlari
-//       const updateUser = {
-//         password: hashedPassword,
-//         phone: data.phone ?? oldUser.phone,
-//       };
+      // Foydalanuvchining profili mavjudligini tekshirish
+      let profile = await prisma.profile.findUnique({
+        where: { user_id: user.id },
+      });
 
-//       const updateProfile = {
-//         address: data.address ?? profile.address,
-//         ava_path: data.ava_path ?? profile.ava_path,
-//       };
-//       console.log(updateUser, updateProfile);
+      // Profil mavjud bo'lmasa, yangi profil yaratish
+      if (!profile) {
+        console.log("profile #######");
 
-//       // Transaktsiya orqali ma'lumotlarni yangilash
-//       const [updatedUser, updatedProfile] = await prisma.$transaction([
-//         prisma.user.update({
-//           where: { id: user.id },
-//           data: updateUser,
-//         }),
-//         prisma.profile.update({
-//           where: { user_id: user.id },
-//           data: updateProfile,
-//         }),
-//       ]);
+        await prisma.profile.create({
+          data: {
+            user_id: Number(user.id),
+            registered_at: new Date().toISOString(),
+          },
+        });
+      }
 
-//       return { updatedUser, updatedProfile };
-//     } catch (error) {}
-//   },
-// };
+      // Parolni xeshlash (agar yangi parol yuborilgan bo'lsa)
+      const hashedPassword = data.password
+        ? await hashPassword(data.password)
+        : oldUser.password;
+
+      // Yangilash ma'lumotlari
+      let updateUser = {
+        password: hashedPassword ?? oldUser.password,
+        phone: data.phone ?? oldUser.phone,
+        address: data.address ?? oldUser.address ?? "toldirilmagan",
+        ava_path: data.ava_path ?? oldUser.ava_path,
+        lastname: data.lastname ?? oldUser.lastname,
+        firstname: data.firstname ?? oldUser.firstname,
+      };
+
+      const newUserInfo = await prisma.user.update({
+        where: { id: user.id },
+        data: updateUser,
+      });
+      console.log("after update user in here");
+
+      return newUserInfo;
+    } catch (err) {
+      if (err instanceof ZodError) {
+        const validationError = err.errors.map((e) => e.message).join(", ");
+        throw new sendError(`Validation Error: ${validationError}`, 400);
+      }
+      throw err;
+    }
+  },
+};
