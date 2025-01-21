f745087ecb03b4726a4a273a4de3b65c346e7efd
Yoqubjonov Dilshodbek <yoqubjonovdilshodo4@gmail.com>
2025-01-21 19:21:23 +0500

    add middleware for session management and token verification

---
+++ src/middlewares/session.middleware.ts
@@ -0,0 +1,43 @@
+import { getEnvVariable, verify } from "@utils";
+import { NextFunction, Request, Response } from "express";
+import Redis from "ioredis";
+
+const redis = new Redis();
+
+export const sessionMiddleware = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<any> => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    return res.status(401).send({ message: "Unauthorized" });
+  }
+
+  try {
+    const token = authHeader.split(" ")[1];
+
+    const decoded = verify(token);
+
+    let storedToken: string | null = null;
+    if (decoded.payload) {
+      const userId = decoded.payload.id;
+      storedToken = await redis.get(`user:${userId}`);
+      console.log(storedToken);
+    } else {
+      console.log("Token yaroqsiz yoki muddati tugagan.");
+      return res.status(401).send({ message: "Unauthorized" });
+    }
+
+    if (storedToken !== token) {
+      return res.status(401).send({ success: false, message: "Unauthorized" });
+    }
+
+    req.user = decoded.payload;
+
+    next();
+  } catch (error) {
+    return res.status(401).send({ message: "Unauthorized" });
+  }
+};
