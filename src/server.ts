1c0144c88115581e05c595f8663aa96624531540
Yoqubjonov Dilshodbek <yoqubjonovdilshodo4@gmail.com>
2025-01-25 19:15:28 +0500

    refactor: simplify CORS middleware configuration in server.ts

--- src/server.ts
+++ src/server.ts
@@ -19,18 +19,8 @@ app.use(
     credentials: true,
   })
 );
-app.options("*", cors());
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:5174"); // update to match the domain you will make the request from
-  res.header(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.header("Access-Control-Allow-Credentials", "true");
-  next();
-});
+app.options("*", cors());
 
 app
   .use("/api/v1/category", categoryRouter)
