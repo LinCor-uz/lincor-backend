32ebf78b5d755bd5e462e6b34ce428e85879641a
Yoqubjonov Dilshodbek <yoqubjonovdilshodo4@gmail.com>
2025-02-06 20:39:14 +0500

    update dependencies and modify jwt sign function parameter type

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -5,10 +5,10 @@ const SECRET_KEY: any =
   getEnvVariable("SECRET_KEY") || "default2_secret32_12#23__key";
 
 // expiresIn parametrini string yoki number deb belgilash mumkin.
-export const sign = (payload: object, expiresIn: string | number): string => {
+export const sign = (payload: object, expiresIn: any): string => {
   const token = jwt.sign(payload, SECRET_KEY, {
-    algorithm: "HS256",
     expiresIn: expiresIn,
+    algorithm: "HS256",
   });
 
   return token;
