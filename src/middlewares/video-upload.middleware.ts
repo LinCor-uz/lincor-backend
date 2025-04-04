ec2f3dc0056a38e3a9fa8ed25eebbeec243ccda6
Yoqubjonov Dilshodbek <yoqubjonovdilshodo4@gmail.com>
2025-04-04 12:54:27 +0500

    added 2048MB limit to vidoe upload size

--- src/middlewares/video-upload.middleware.ts
+++ src/middlewares/video-upload.middleware.ts
@@ -3,28 +3,28 @@ import * as fs from "fs";
 import path from "path";
 import dotenv from "dotenv";
 
-dotenv.config(); // 📌 `.env` faylni yuklash
+dotenv.config(); // .env faylni yuklash
 
 const videoDir = path.join(__dirname, "../../uploads/video");
 
-// ✅ **Agar direktoriy yo‘q bo‘lsa, yaratish**
+// Agar direktoriy yo‘q bo‘lsa, yaratish
 if (!fs.existsSync(videoDir)) {
   fs.mkdirSync(videoDir, { recursive: true });
 }
 
-// ✅ **Ruxsat berilgan fayl turlari**
+// Ruxsat berilgan fayl turlari
 const allowedMimeTypes = ["video/mp4", "video/webm", "video/x-msvideo", "video/x-matroska"];
 
-// ✅ **Fayl filtrini o‘rnatish**
+// Fayl filtrini o‘rnatish
 const fileFilter = (req: any, file: Express.Multer.File, cb: multer.FileFilterCallback) => {
-  console.log("📂 Fayl yuklanmoqda:", file.originalname);
+  console.log(" Fayl yuklanmoqda:", file.originalname);
   if (!allowedMimeTypes.includes(file.mimetype)) {
     return cb(Error as any, false);
   }
   cb(null, true);
 };
 
-// ✅ **Faylni saqlash va nomlash**
+// Faylni saqlash va nomlash
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, videoDir);
@@ -35,5 +35,11 @@ const storage = multer.diskStorage({
   },
 });
 
-// ✅ **Multer middleware'ni eksport qilish**
-export const uploadVideo = multer({ storage, fileFilter });
+// Multer middleware'ni eksport qilish
+export const uploadVideo = multer({ 
+  storage, 
+  fileFilter,
+  limits: {
+    fileSize: 2048 * 1024 * 1024 // 50MB limit
+  }
+});
